4a92a3ee23ca77aef20fc9331f81c0b211456eb2
Maria Delfina <mfurio@itba.edu.ar>
2023-11-20 15:01:52 -0300

    commit

--- javascript.js
+++ javascript.js
@@ -20,3 +20,46 @@
 // document.getElementByaId("tituloprinicipal").innerHTML=
 // "hola nombra";
 
+
+
+let total = 0;
+let cantidad = 0;
+
+function comprar(valorproducto) {
+  total = total + valorproducto;
+  document.getElementById("total-a-pagar").innerHTML = "$" + total;
+  cantidad += 1;
+
+  document.getElementById("cantidad").innerHTML = cantidad;
+  document.getElementById("no-productos").innerHTML = " ";
+}
+
+function descomprar(valorproducto) {
+  if (cantidad > 0) {
+    total = total - valorproducto;
+    document.getElementById("total-a-pagar").innerHTML = "$" + total;
+    cantidad -= 1;
+    document.getElementById("cantidad").innerHTML = cantidad;
+    document.getElementById("no-productos").innerHTML = " ";
+  }
+}
+
+function vaciar() {
+  total = 0;
+  document.getElementById("total-a-pagar").innerHTML = " $" + total;
+  document.getElementById("no-productos").innerHTML =
+    "No hay productos en el carrito.";
+  cantidad = 0;
+  document.getElementById("cantidad").innerHTML = cantidad;
+}
+
+function validarCaptcha() {
+  var response = grecaptcha.getResponse();
+  if (response.length === 0) {
+    console.log("Por favor, completa el captcha.");
+    return false;
+  } else {
+    return true;
+  }
+}
+
